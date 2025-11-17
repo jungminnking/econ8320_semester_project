096139cb4bc17b5855bfcbc28347a22a1e9c9d32
jungminnking <jungminnhwang@gmail.com>
2025-11-17 11:47:49 -0600

    push

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,8 @@ def load_data(url: str) -> pd.DataFrame:
     df = pd.read_csv(url, parse_dates=["date"])
     df["series_id"] = df["series_id"].astype("string")
     return df
-    
-csv_url = "https://github.com/jungminnking/econ8320_semester_project/main/data/bls_timeseries.csv"
+
+csv_url = "https://raw.githubusercontent.com/jungminnking/econ8320_semester_project/main/data/bls_timeseries.csv"    
 df_all = load_data(csv_url)
 
 # Series
